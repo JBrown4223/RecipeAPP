730e96d9ac9f1cec0ea74a7aff9172859785cb28
JBrown4223 <49790807+JBrown4223@users.noreply.github.com>
2022-02-17 15:41:40 -0500

    Update config.js
    Removed sensitive data

--- server/config/config.js
+++ server/config/config.js
@@ -1,11 +1,6 @@
 export const PORT = 8080;
 export const environment = {
     development: {
-        serverURL: `http://localhost:${PORT}/`,
-        dbString: 'mongodb+srv://dbUser2:Clippers@cluster0.92fl8.mongodb.net/RecipeAppDB?retryWrites=true&w=majority'
-    },
-    production: {
-        serverURL: `http://localhost:${PORT}/`,
-        dbString: 'mongodb+srv://dbUser2:Clippers@cluster0.92fl8.mongodb.net/RecipeAppDB?retryWrites=true&w=majority'
+        "Secret Manager or DB Connection String would go here"
     }
 }
